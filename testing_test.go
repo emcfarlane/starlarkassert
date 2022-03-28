c4e2b481aac22e4f58a0cb788c2c7dc52ca7a4eb
Edward McFarlane <emcfarlane000@gmail.com>
2022-03-28 15:33:51 +0100

    Remove test filename, tooling issues

--- testing_test.go
+++ testing_test.go
@@ -1,8 +1,37 @@
 package starlarkassert
 
-import "testing"
+import (
+	"testing"
 
-func _check() {
+	"go.starlark.net/starlark"
+	"go.starlark.net/starlarkstruct"
+)
+
+func TestRunTests(t *testing.T) {
+	globals := starlark.StringDict{
+		"struct": starlark.NewBuiltin("struct", starlarkstruct.Make),
+	}
+	opt := func(_ testing.TB, thread *starlark.Thread) func() {
+		originalLoad := thread.Load
+		thread.Load = func(thread *starlark.Thread, module string) (starlark.StringDict, error) {
+			switch module {
+			case "test_load.star":
+				return starlark.StringDict{
+					"greet": starlark.String("world"),
+				}, nil
+			}
+			return originalLoad(thread, module)
+		}
+		return func() {
+			thread.Load = originalLoad
+		}
+	}
+
+	RunTests(t, "testdata/*.star", globals, opt)
+}
+
+func Test_depsInterface(t *testing.T) {
+	t.Skip() // Just check it compiles
 	var deps MatchStringOnly = nil
 	testing.MainStart(deps, nil, nil, nil, nil)
 }
